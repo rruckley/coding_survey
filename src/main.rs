d4a17f3c53ef52e6fbe3e7e60d669e62a567c5a4
Ryan Ruckley <rruckley@gmail.com>
2024-07-09 13:07:48 +1000

    Extract common languages into CSV

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,8 @@ use log::{info,error};
 use gitlab::Gitlab;
 use gitlab::api::{raw, self, projects, Client, Endpoint, Query, RestClient};
 use serde::{Deserialize};
+use serde_json::Result;
+use serde_json::value;
 use std::collections::HashMap;
 
 #[derive(Debug, Deserialize)]
@@ -10,6 +12,20 @@ struct Project {
     id: u16,
     name: String,
     _links: HashMap<String,String>,
+}
+
+#[derive(Debug,Deserialize)]
+struct Languages {
+    Go: Option<f32>,
+    Java: Option<f32>,
+    Kotlin: Option<f32>,
+    MakeFile: Option<f32>,
+    Dockerfile: Option<f32>,
+    TypeScript: Option<f32>,
+    CSS: Option<f32>,
+    HTML: Option<f32>,
+    Shell:Option<f32>,
+    Python:Option<f32>,
 }
 
 fn main() {
@@ -30,9 +46,9 @@ fn main() {
 
     info!("Starting {} v{}",pkg,ver);
 
-    info!("Using Gitlab: {}",gitlab_host);
+    info!("Using Gitlab: {}",&gitlab_host);
 
-    let client =Gitlab::new(gitlab_host, gitlab_token).unwrap();
+    let client =Gitlab::new(gitlab_host.clone(),gitlab_token.clone()).unwrap();
 
     let endpoint =projects::Projects::builder()
         .build()
@@ -41,8 +57,47 @@ fn main() {
         .query(&client)
         .unwrap();
 
+    let mut output : HashMap<String,Languages> = HashMap::default();
     info!("Found {} projects",first_200.len());
     for e in first_200 {
         info!("Pulling language info: {} [{}]",e.name,e.id);
+        let url = format!("https://{}/api/v4/projects/{}/languages",gitlab_host,e.id);
+        // info!("Calling url: {}",url);
+        let client = reqwest::blocking::Client::new();
+        let response = client
+            .get(url)
+            .header("Authorization", gitlab_token.clone())
+            .header("PRIVATE-TOKEN",gitlab_token.clone())
+            .send();
+        match response {
+            Ok(r) => {
+                
+                let body = r.text().unwrap();
+                // info!("Got a response: {}",&body);
+                let obj : Languages = serde_json::from_str(body.as_str()).unwrap();
+                // dbg!(&obj);
+                output.insert(e.name, obj);
+            }
+            Err(e) => {
+                error!("Could not get response: {}",e);
+            }
+        }
     }
+    // Output to CSV
+    println!("Project,Go,Java,Kotlin,MakeFile,Dockerfile,TypeScript,CSS,HTML,Schell,Python");
+    for (name,lang) in output {
+        println!("{},{},{},{},{},{},{},{},{},{},{}",
+            name,
+            lang.Go.unwrap_or_default(),
+            lang.Java.unwrap_or_default(),
+            lang.Kotlin.unwrap_or_default(),
+            lang.MakeFile.unwrap_or_default(),
+            lang.Dockerfile.unwrap_or_default(),
+            lang.TypeScript.unwrap_or_default(),
+            lang.CSS.unwrap_or_default(),
+            lang.HTML.unwrap_or_default(),
+            lang.Shell.unwrap_or_default(),
+            lang.Python.unwrap_or_default(),
+        );
+    };
 }
